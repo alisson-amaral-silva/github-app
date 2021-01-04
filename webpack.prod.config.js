bf14899529177d5f9498c851815644c5e310e05d
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2021-01-04 09:57:09 -0300

    webpack prod

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,25 +3,30 @@
 const path = require('path');
 const webpack = require('webpack');
 const validate = require('webpack-validator');
+
 const HtmlPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = validate({
-    devtool: 'source-map',
-    entry: [
-        'react-hot-loader/patch',
-        'webpack-dev-server/client?http://localhost:3000',
-        'webpack/hot/only-dev-server',
-        path.join(__dirname, 'src', 'index')
-    ],
+    entry: path.join(__dirname, 'src', 'index'),
     output: {
         path: path.join(__dirname, 'dist'),
         filename: '[name]-[hash].js',
-        publicPath: ''
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),
         new ExtractTextPlugin('style.css'),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
         new HtmlPlugin({
             title: 'GitHub App',
             template: path.join(__dirname, 'src', 'html', 'template.html')
@@ -39,11 +44,11 @@ module.exports = validate({
             exclude: /node_modules/,
             include: /src/,
             loader: 'babel'
-        },{
+        }, {
             test: /\.css$/,
             exclude: /node_modules/,
             include: /src/,
-            loaders: ['style', 'css']
+            loaders: ExtractTextPlugin.extract('style', 'css')
         }]
     }
     // devServer: {
